c4e805f0fa4477c399464b4468af9ac9d0745ee5
johnmmoss <johnmmoss@gmail.com>
2016-03-22 21:10:20 +0000

    Added the BlockingCollectionArrayPipeline example into the sln

--- src/BlockingCollectionArrayPipeline2/Program.cs
+++ src/BlockingCollectionArrayPipeline2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -91,79 +90,6 @@ namespace BlockingCollectionArrayPipeline2
             // once for the cancellation thread, and once for this thread.
             Console.WriteLine("Press any key.");
             Console.ReadKey(true);
-        }
-
-        class PipelineFilter<TInput, TOutput>
-        {
-            public BlockingCollection<TInput>[] m_input;
-            public BlockingCollection<TOutput>[] m_output = null;
-            
-            Func<TInput, TOutput> m_processor = null;
-            Action<TInput> m_outputProcessor = null;
-            
-            public string Name { get; private set; }
-            CancellationToken m_token;
-
-            public PipelineFilter(
-                BlockingCollection<TInput>[] input,
-                Func<TInput, TOutput> processor,
-                CancellationToken token,
-                string name)
-            {
-                m_input = input;
-                m_output = new BlockingCollection<TOutput>[5];
-                for (int i = 0; i < m_output.Length; i++)
-                    m_output[i] = new BlockingCollection<TOutput>(500);
-
-                m_processor = processor;
-                m_token = token;
-                Name = name;
-            }
-
-            // Use this constructor for the final endpoint, which does
-            // something like write to file or screen, instead of
-            // pushing to another pipeline filter.
-            public PipelineFilter(
-                BlockingCollection<TInput>[] input,
-                Action<TInput> renderer,
-                CancellationToken token,
-                string name)
-            {
-                m_input = input;
-                m_outputProcessor = renderer;
-                m_token = token;
-                Name = name;
-            }
-
-            public void Run()
-            {
-                Console.WriteLine("{0} is running", this.Name);
-                while (!m_input.All(bc => bc.IsCompleted) && !m_token.IsCancellationRequested)
-                {
-                    TInput receivedItem;
-                    int i = BlockingCollection<TInput>.TryTakeFromAny(
-                        m_input, out receivedItem, 50, m_token);
-                    if (i >= 0)
-                    {
-                        if (m_output != null) // we pass data to another blocking collection
-                        {
-                            TOutput outputItem = m_processor(receivedItem);
-                            BlockingCollection<TOutput>.AddToAny(m_output, outputItem);
-                            Console.WriteLine("{0} sent {1} to next", this.Name, outputItem);
-                        }
-                        else // we're an endpoint
-                        {
-                            m_outputProcessor(receivedItem);
-                        }
-                    }
-                    else
-                        Console.WriteLine("Unable to retrieve data from previous filter");
-                }
-                if (m_output != null)
-                {
-                    foreach (var bc in m_output) bc.CompleteAdding();
-                }
-            }
         }
     }
 }
